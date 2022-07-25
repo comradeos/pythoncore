a0cc6251cecd7292dc8638e87acb6f20990a0a37
Iaroslav Os <osmnbx@gmail.com>
2022-07-25 20:31:03 +0300

    def. functions

--- cs50py1/py01_var.py
+++ cs50py1/py01_var.py
@@ -5,4 +5,8 @@ print(f'{1000000:,}') # 1,000,000
 print(f'{1/2}') # 0.5
 print(f'{1/2:.2f}') # 0.50
 
+def hello(name:str='world')->None:
+    print(f'hello,', name)
 
+hello() # hello, world
+hello('bob') # hello, bob
