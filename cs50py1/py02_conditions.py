86db062a7bf2d06b52899dbd34970f87e1722c30
Iaroslav Os <osmnbx@gmail.com>
2022-07-26 23:32:24 +0300

    added a<b<c

--- cs50py1/py02_conditions.py
+++ cs50py1/py02_conditions.py
@@ -1,6 +1,7 @@
 x = 10
 y = 11
 
+
 if x > y:
     print('x > y')
 elif x == y:
@@ -9,3 +10,7 @@ elif x < y:
     print('x < y')
 else:
     print('What?')
+
+
+print(10 < 20 and 20 < 30)
+print(10 < 20 < 30)
