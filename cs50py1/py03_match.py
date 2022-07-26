47faacb318f75a2134bbcf2c4e3aa20d0cb83fb5
Iaroslav Os <osmnbx@gmail.com>
2022-07-27 00:07:26 +0300

    added match case

---
+++ cs50py1/py03_match.py
@@ -0,0 +1,23 @@
+
+name = input('What is your name? ')
+
+if name == 'Harry':
+    print('Gryffindor')
+elif name == 'Hermione':
+    print('Gryffindor')
+elif name == 'Ron':
+    print('Gryffindor')
+elif name == 'Draco':
+    print('Slytherin')
+else:
+    print('Who?')
+
+
+match name:
+    case 'Harry' | 'Hermione' | 'Ron':
+        print('Gryffindor')
+    case 'Draco':
+        print('Slytherin')
+    case other:
+        print(f'Who is {other}?')    
+        
