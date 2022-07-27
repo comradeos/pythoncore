99a82ffe63b7e33e3838099df6f05aa22eb5d316
Iaroslav Os <osmnbx@gmail.com>
2022-07-27 22:13:14 +0300

    upd loops file

--- cs50py1/py04_loops.py
+++ cs50py1/py04_loops.py
@@ -2,3 +2,23 @@ i = 3
 while i != 0:
     print('meow')
     i -= 1
+
+
+print()
+
+
+for i in [1, 2, 3]:
+    print('meow')
+
+
+print()
+
+
+for i in range(3):
+    print('meow')
+
+
+print()
+
+
+print('meow\n' * 3)
