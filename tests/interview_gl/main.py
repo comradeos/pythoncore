ae09a4bd7660dc9e3d2a40bf1d412b5365169c98
Iaroslav Os <osmnbx@gmail.com>
2022-10-14 18:33:12 +0300

    add/upd

--- tests/interview_gl/main.py
+++ tests/interview_gl/main.py
@@ -397,3 +397,19 @@ b = MyClassB(3,5)
 # print(b._MyClassB__private) # ошибка
 
 
+
+class Shark():
+    def swim(self):
+        print("The shark is swimming.")
+    def swim_backwards(self):
+        print("The shark cannot swim backwards, but can sink backwards.")
+    def skeleton(self):
+        print("The shark's skeleton is made of cartilage.")
+        
+class Clownfish():
+    def swim(self):
+        print("The clownfish is swimming.")
+    def swim_backwards(self):
+        print("The clownfish can swim backwards.")
+    def skeleton(self):
+        print("The clownfish's skeleton is made of bone.")
