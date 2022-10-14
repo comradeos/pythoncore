8c33ce4b4f5c2d39c94d0892e5ca6c649cb5a661
Iaroslav Os <osmnbx@gmail.com>
2022-10-14 22:22:56 +0300

    add/upd

--- tests/interview_gl/another.py
+++ tests/interview_gl/another.py
@@ -7,21 +7,38 @@ class C1:
 c1 = C1(1, 2, 3, a=1, b=2, c=3)
 print(c1.args, c1.kwargs)
 
-a = set([1, 2, 3, 3, 3, 3, 3])
+a = {1, 2, 3, 3, 3, 3, 3}
 a.add(9)
 print(a)
 b = frozenset([1, 2, 3, 3, 3, 3, 3])
 print(b)
 
-from typing import List, TypeVar, Any
+from typing import TypeVar, Generic, List
+
+T = TypeVar('T')
+print(T)
 
 
-def first(container: List[Any]) -> Any:
-    return container[0]
+from typing import TypeVar, Generic, List
+
+T = TypeVar('T')
+
+class Stack(Generic[T]):
+    def __init__(self) -> None:
+        # Create an empty list with items of type T
+        self.items: List[T] = []
+
+    def push(self, item: T) -> None:
+        self.items.append(item)
+
+    def pop(self) -> T:
+        return self.items.pop()
+
+    def empty(self) -> bool:
+        return not self.items
 
 
-list_one: List[str] = ["a", "b", "c"]
-print(first(list_one))
-
-list_two: List[int] = [1, 2, 3]
-print(first(list_two))
+stack = Stack[int]()
+stack.push(2)
+stack.pop()
+stack.push('x')
