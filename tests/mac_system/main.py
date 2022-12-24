b07bb7a24663de024dc525c36721bf5e244186a9
Iaroslav Os <osmnbx@gmail.com>
2022-12-24 09:44:31 +0200

    added/updated

--- tests/mac_system/main.py
+++ tests/mac_system/main.py
@@ -7,11 +7,3 @@ print(__name__)
 print(os.path.dirname(__file__))
 
 say_hello()
-
-
-
-
-def contribute_something(value_1=1, value_2=1)->int:
-    return value_1+value_2
-
-print(contribute_something("sds",2))
