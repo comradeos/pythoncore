03ab87546d5eab31376849e1d50b134d84823d83
Iaroslav Os <osmnbx@gmail.com>
2023-03-27 20:18:53 +0300

    added/updated

---
+++ py004innapy/les_02_data_functions.py
@@ -0,0 +1,3 @@
+def lesson02():
+    a = 7
+    print(a)
--- py004innapy/main.py
+++ py004innapy/main.py
@@ -1,5 +1,7 @@
 from les_01_hello_world import *
+from les_02_data_functions import *
 
 if __name__ == '__main__':
-    print('main file')
-    lesson01()
+    # print('main file')
+    # lesson01()
+    lesson02()
