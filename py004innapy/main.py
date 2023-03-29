3d9242f970dc478a7e5ead4d7407ed300b6fd739
Iaroslav Os <osmnbx@gmail.com>
2023-03-29 20:09:34 +0300

    added/updated

--- py004innapy/main.py
+++ py004innapy/main.py
@@ -1,7 +1,8 @@
 # import les_01_hello_world
 # import les_02_data_functions
 # import les_03_numbers
-import les_04_math
+# import les_04_math
+import les_05_print_input
 
 if __name__ == '__main__':
     print()
