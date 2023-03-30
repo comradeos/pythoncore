b607efa6b28446e3f6068949dae8fcd6d9c8a129
Iaroslav Os <osmnbx@gmail.com>
2023-03-30 20:41:41 +0300

    added/updated

--- py004innapy/main.py
+++ py004innapy/main.py
@@ -2,7 +2,8 @@
 # import les_02_data_functions
 # import les_03_numbers
 # import les_04_math
-import les_05_print_input
+# import les_05_print_input
+import les_06_bool_and_or_not
 
 if __name__ == '__main__':
     print()
