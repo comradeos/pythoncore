c679463b6025f500470465c5a2a79162cf843b32
Iaroslav Os <osmnbx@gmail.com>
2023-04-03 20:40:18 +0300

    upd

--- py004innapy/main.py
+++ py004innapy/main.py
@@ -3,7 +3,8 @@
 # import les_03_numbers
 # import les_04_math
 # import les_05_print_input
-import les_06_bool_and_or_not
+# import les_06_bool_and_or_not
+import les_07_string_ops
 
 if __name__ == '__main__':
     print()
