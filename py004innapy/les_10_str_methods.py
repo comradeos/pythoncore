4170e99aeb0f253f3a7bae47383e10708cfc923a
Iaroslav Os <osmnbx@gmail.com>
2023-04-09 09:43:07 +0300

    added/updated

--- py004innapy/les_10_str_methods.py
+++ py004innapy/les_10_str_methods.py
@@ -4,4 +4,16 @@ print(s.lower())
 print(s.capitalize())
 
 s = "abracadabra"
-print(s.count("a"))
+print(s.count("ra"))
+print(s.count("ra", 4))
+print(s.count("ra", 4, 5))
+
+print(s.find("br"))
+print(s.find("br", 2))
+
+print(s.rfind("br", 2))
+
+# print(s.index("brf", 2))
+
+s = "abcd abcd abcd"
+print(s.replace("a", "@"))
