06e144700e157904c44a992af805fc5fe0722b1a
Iaroslav Os <osmnbx@gmail.com>
2023-04-11 21:43:02 +0300

    added/updated

--- py004innapy/les_12_string_format.py
+++ py004innapy/les_12_string_format.py
@@ -3,4 +3,5 @@ age = 33
 
 print("My name " + name + ", I\'m " + str(age) + " years old.")
 print("My name {0}, I\'m {1} years old.".format(name, age))
+print("My name {fio}, I\'m {old} years old.".format(fio=name, old=age))
 print(f"My name {name}, I\'m {age} years old.")
