9ef934d38f438e874378f29ca6b04f4ab932aa49
Iaroslav Os <osmnbx@gmail.com>
2023-04-14 20:13:12 +0200

    updated

--- tests/weekend_navigator/main.py
+++ tests/weekend_navigator/main.py
@@ -1,23 +1,50 @@
 import random
+import time
+import webbrowser
 
-with open("loc_d") as file:
-    d = file.read().splitlines()
 
-with open("loc_v") as file:
-    v = file.read().splitlines()
+def get_locations(filename: str) -> list:
+    with open(filename, mode="r", encoding="utf-8") as f:
+        return list(f.read().splitlines())
 
-d_updated = list(set(d)-set(v))
 
-print(d_updated)
+def set_visited(location: str) -> None:
+    with open("locations_visited", mode="a", encoding="utf-8") as f:
+        f.write(f"{location}" + "\n")
 
-try:
-    r = random.choice(d_updated)
-except:
-    print("that's all")
+
+def make_intrigue() -> None:
+    dots_number = random.randint(3, 7)
+    for i in range(dots_number):
+        time.sleep(1)
+        print(".", end='')
+    print("\n")
+
+
+print("Навігатор \"Кудись ХЗ-1\"")
+
+locations_all = get_locations("locations_all")
+locations_visited = get_locations("locations_visited")
+locations_updated = list(set(locations_all) - set(locations_visited))
+
+# print(locations_all)
+# print(locations_visited)
+# print(locations_updated)
+
+if len(locations_updated) == 0:
+    print("Всі маршрути відвідані, час поновити :)")
     exit(0)
 
-print(f"-> {r}")
+print("Обираємо маршрут", end='')
+make_intrigue()
 
-with open("loc_v", mode='a') as file:
-    v = file.write(f"{r}" + "\n")
+random_location = random.choice(locations_updated)
 
+print(f"Маршрут обрано! Сьогодні випало відвідати:\n\t{random_location}")
+set_visited(random_location)
+print()
+
+print(f"Шукаємо інформацію в Інтернеті про {random_location}", end='')
+make_intrigue()
+
+webbrowser.open(f"https://www.google.com/search?q=Київ+\"{random_location}\"+цікаві+місця")
