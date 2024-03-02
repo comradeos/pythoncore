024aae21dc2ad26ac0f71c62441066d7703a949d
Iaroslav Os <osmnbx@gmail.com>
2024-03-02 14:52:47 +0200

    updated

--- py006docker1/src/main.py
+++ py006docker1/src/main.py
@@ -8,7 +8,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 while True:
     print('Hello Docker')
     # Отправляем GET-запрос на страницу
-    response = requests.get(r'https://www.ebay.com/itm/185997734023')
+    response = requests.get(r'https://www.ebay.com/itm/364577067218')
 
     # Используем регулярное выражение для поиска цены
     price = re.search(r'US \$([0-9,\.]+)', response.text)
